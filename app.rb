c72b307d347a8f1cc39aeba5768f65704b97ba30
Andrew Giang <giang184@gmail.com>
2021-07-30 11:56:32 -0700

    set up homepage

--- app.rb
+++ app.rb
@@ -3,11 +3,20 @@ require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require 'pry'
 require "pg"
-require('./lib/album')
-require('./lib/song')
+require('./lib/project')
+require('./lib/volunteer')
 
 DB = PG.connect({ dbname: 'volunteer_tracker', host: 'db', user: 'postgres', password: 'password' })
 
 get '/' do
   redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get ('/trains/new') do
+  erb(:new_project)
 end
