4824afbc627f5b7854671e2d189801e9018f6bc1
Andrew Giang <giang184@gmail.com>
2021-07-30 11:03:05 -0700

    pass test 10

--- lib/project.rb
+++ lib/project.rb
@@ -38,8 +38,8 @@ class Project
     Project.new({:title => title, :id => id})
   end
 
-  def update(title)
-    @title = title
+  def update(attributes)
+    @title = attributes.fetch(:title)
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 
